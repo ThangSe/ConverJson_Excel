766b26e86dff5453783da4466609bf08a56ff81e
ThangSe <thangvhv99@gmail.com>
2023-07-09 19:47:47 +0700

    update

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ var fs = require("fs")
 
 const app = express()
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 app.use(bodyParser.json({limit:"50mb"}))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get("/JsonExportExcel", async (req, res) => {
+app.post("/JsonExportExcel/Convert-All", async (req, res) => {
     try {
         let object
         let workbook = new excelJs.Workbook()
-        let row = req.body.ImageStartPos.split('')[0]
-        let column = req.body.ImageStartPos.split('')[1]
+        let row = 1
         try{
             workbook = await workbook.xlsx.readFile(req.body.ExcelPath);
         }   
         catch (err){
-            throw new Error("Excel file not found " + req.body.ExcelPath)        
+            throw new Error("Excel file not found at path " + req.body.ExcelPath)        
         }
         
         const sheet = workbook.addWorksheet(req.body.SheetName)
@@ -35,9 +34,9 @@ app.get("/JsonExportExcel", async (req, res) => {
             object = JSON.parse(fs.readFileSync(req.body.ConfigJsonPath, 'utf8'))
         }
         catch(err) {
-            throw new Error("Config file not found " + req.body.ConfigJsonPath)
+            throw new Error("Config file not found at path " + req.body.ConfigJsonPath)
         }
-        await object.map((value) => {
+        await object.map((value, index) => {
             const imageId = workbook.addImage({
                 filename: `${req.body.ImagePath}${value.FileName}`,
                 extension: 'png',
@@ -49,8 +48,80 @@ app.get("/JsonExportExcel", async (req, res) => {
                 type: value.type,
                 category: value.Category,
             })
-            sheet.addImage(imageId, `${row}${column}:${row}${column}`)
-            column++;
+            sheet.addImage(imageId, {
+                tl: {col: sheet.getColumn("image").number - 1, row: row},
+                ext: {width: 100, height: 20},
+                editAs: 'undefined'
+            })
+            row++
+        })
+        await workbook.xlsx.writeFile(req.body.ExcelPath);
+        res.status(200).json("Success");
+
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+app.post("/JsonExportExcel/Edit-Exist", async (req, res) => {
+    try {
+        let object
+        let sheet
+        var nextPosDataRow = req.body.StartPosDataRow
+        let workbook = new excelJs.Workbook()
+        try{
+            workbook = await workbook.xlsx.readFile(req.body.ExcelPath);
+        }   
+        catch (err){
+            throw new Error("Excel file not found at path " + req.body.ExcelPath)        
+        }
+        try {
+            sheet = await workbook.getWorksheet(req.body.SheetName)
+            if(!sheet) throw new Error()
+        } 
+        catch (err) {
+            throw new Error("Sheet name " + req.body.SheetName + " does not existed")
+        }
+
+        const rowCount = sheet.rowCount
+        sheet.columns = [
+            {header: "Id", key: "id", width: 5},
+            {header: "FileName", key: "filename", width: 25},
+            {header: "Image", key: 'image', width: 15},
+            {header: "Object", key: "object", width: 25},
+            {header: "Category", key: "category", width: 25},
+            {header: "Type", key: "type", width: 25},
+        ]  
+        try{
+            object = JSON.parse(fs.readFileSync(req.body.ConfigJsonPath, 'utf8'))
+        }
+        catch(err) {
+            throw new Error("Config file not found at path " + req.body.ConfigJsonPath)
+        }   
+        await object.map((value) => {
+            const imageId = workbook.addImage({
+                filename: `${req.body.ImagePath}${value.FileName}`,
+                extension: 'png',
+            })
+            for (var i = nextPosDataRow; i <= rowCount; i++) {
+                const row = sheet.getRow(i)
+                if(row.values[1] == value.Id) {
+                    row.values = {
+                        id: value.Id,
+                        filename: value.FileName,
+                        object: value.object,
+                        type: value.type,
+                        category: value.Category,       
+                    }
+                    sheet.addImage(imageId, {
+                        tl: {col: sheet.getColumn("image").number - 1, row: row.number - 1},
+                        ext: {width: 100, height: 20},
+                        editAs: 'undefined'
+                    })
+                    nextPosDataRow++
+                    break
+                }
+            }
         })
         await workbook.xlsx.writeFile(req.body.ExcelPath);
         res.status(200).json("Success");
